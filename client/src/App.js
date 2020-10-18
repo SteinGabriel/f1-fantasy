6b40869296953fa4dbd6e8c5ea328d7ae589e726
Gabriel Stein <steindleve@gmail.com>
2020-10-17 23:44:35 -0700

    implement frontend drivers overview request

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+
+const ROOT_URL = 'http://localhost:8000'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getDriversOverview = function () {
+    axios.get(ROOT_URL + '/getDriversOverview')
+      .then(response => {
+        setIsLoading(false)
+        console.log(response);
+      })
+      .catch(err => console.warn(`Error: ${err}`))
+  }
+
+  useEffect(() => {
+    getDriversOverview()
+  }, [])
+
   return (
     <div className="App">
+      {isLoading && <span>...loading</span>}
     </div>
   );
 }
--- server/controllers/index.js
+++
@@ -1,5 +0,0 @@
-// This file is the entry point to the controllers.
-// Controllers are files where the backend logic is held. It's 
-// the place to add interactions with the db, transform data and 
-// any backend work the app needs to do before returning (or not) 
-// to the user. 
