c408c612b7fdc6d517d317e94bbfd0ed3b36cac5
Gabriel Stein <steindleve@gmail.com>
2020-10-17 23:45:19 -0700

    add cors config to the server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const port = 8000
+const cors = require('cors')
 
-// Add routes to root endpoints.
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Add cors whitelist
+app.use(cors(corsOptions))
+// Add routes to root endpoint.
 app.use('/', routes)
 
 // Starts a server, serving backend 
