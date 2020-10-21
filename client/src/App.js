2e9ae0e9a9f7ff91d53b884d730e51f6363c8f3d
Gabriel Stein <steindleve@gmail.com>
2020-10-21 13:12:02 -0700

    remove base.scss import from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import axios from 'axios'
 import DriversOverviewTable from './components/driversOverviewTable/driversOverviewTable'
 
